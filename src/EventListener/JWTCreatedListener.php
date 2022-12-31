45115f9acc5710bc5bac525153e444236cb60dca
guclusefa <sefaaguclu@gmail.com>
2022-12-31 12:01:03 +0100

    remember me

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -3,19 +3,35 @@
 namespace App\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class JWTCreatedListener
 {
+    const REMEMBER_ME_EXPIRATION_DAYS = 30;
+    public function __construct(private readonly RequestStack $requestStack)
+    {
+    }
+
     public function onJWTCreated(JWTCreatedEvent $event): void
     {
         $payload = $event->getData();
+        // user data
         $user = $event->getUser();
         $payload['id'] = $user->getId();
         $payload['username'] = $user->getUsername();
         $payload['isVerified'] = $user->isIsVerified();
         $payload['isActive'] = $user->isIsActive();
         $payload['isBanned'] = $user->isIsBanned();
+        // remember me
+        $request = $this->requestStack->getCurrentRequest();
+        $content = json_decode($request->getContent(), true);
+        if (isset($content['remember_me']) && $content['remember_me']) {
+            $expiration = new \DateTimeImmutable(sprintf('+%d days', self::REMEMBER_ME_EXPIRATION_DAYS));
+            $payload['exp'] = $expiration->getTimestamp();
+        }
+        // update token
         $event->setData($payload);
-
+        // add header
         $header = $event->getHeader();
         $header['cty'] = 'JWT';
         $event->setHeader($header);
