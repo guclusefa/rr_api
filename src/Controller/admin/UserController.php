1795eca4ad29e55ca95ab5621b5ec605413d0bb0
guclusefa <sefaaguclu@gmail.com>
2023-01-27 13:33:11 +0100

    certify ban & upgrade user

--- src/Controller/admin/UserController.php
+++ src/Controller/admin/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/api/admin/users')]
@@ -40,6 +41,47 @@ class UserController extends AbstractController
         // return
         return new JsonResponse(
             ['message' => $message],
+            Response::HTTP_OK
+        );
+    }
+
+    #[Route('/{id}/ban', name: 'api_admin_users_ban', methods: ['POST'])]
+    public function ban(User $user): JsonResponse
+    {
+        // ban
+        $isBanned = $user->isIsBanned();
+        $user->setIsBanned(!$isBanned);
+        if ($isBanned) {
+            $message = $this->translator->trans('message.user.unban_success');
+        } else {
+            $message = $this->translator->trans('message.user.ban_success');
+        }
+        $this->userRepository->save($user, true);
+        // return
+        return new JsonResponse(
+            ['message' => $message],
+            Response::HTTP_OK
+        );
+    }
+
+    #[Route('/{id}/upgrade/{role}', name: 'api_admin_users_upgrade', methods: ['POST'])]
+    #[IsGranted('ROLE_SUPER_ADMIN')]
+    public function upgrade(User $user, $role): JsonResponse
+    {
+        // valid role
+        $role = match ($role) {
+            '1' => ['ROLE_MODERATOR'],
+            '2' => ['ROLE_ADMIN'],
+            '3' => ['ROLE_SUPER_ADMIN'],
+            default => [],
+        };
+        // upgrade
+        $user->setRoles($role);
+        // save
+        $this->userRepository->save($user, true);
+        // return
+        return new JsonResponse(
+            ['message' => $this->translator->trans('message.user.upgrade_success')],
             Response::HTTP_OK
         );
     }
