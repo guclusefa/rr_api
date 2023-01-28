875278b01e7f8af6e106e6fde90b919ef00704cb
guclusefa <sefaaguclu@gmail.com>
2023-01-28 13:33:54 +0100

    local listener bug

--- src/EventListener/JWTDecodedListener.php
+++ src/EventListener/JWTDecodedListener.php
@@ -3,14 +3,14 @@
 namespace App\EventListener;
 
 use App\Entity\User;
-use App\Entity\UserBan;
-use App\Repository\UserBanRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Translation\Translator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class JWTDecodedListener
@@ -19,15 +19,24 @@ class JWTDecodedListener
         private readonly RequestStack $requestStack,
         private readonly EntityManagerInterface $entityManager,
         private readonly TranslatorInterface $translator,
-        private readonly UserRepository $userRepository
+        private readonly UserRepository $userRepository,
+        // needed because of locale listener
+        private readonly ParameterBagInterface $params
     )
     {
     }
     public function onJWTDecoded(JWTDecodedEvent $event): void
     {
+        // set locale because dosent work for some reason in LocaleListener : needs to be fixed
         $request = $this->requestStack->getCurrentRequest();
-        $payload = $event->getPayload();
+        if ($request->headers->has('Accept-Language')) {
+            $locale = $request->headers->get('Accept-Language');
+            if (in_array($locale, $this->params->get('app.supported_locales'))) {
+                $this->translator->setLocale($request->headers->get('Accept-Language'));
+            }
+        }
 
+        $payload = $event->getPayload();
         // if user is banned
         $user = $this->entityManager->getRepository(User::class)->find($payload['id']);
         if ($user !== null && $this->userRepository->isBanned($user)) {
@@ -38,13 +47,13 @@ class JWTDecodedListener
             $reason = $ban->getReason();
             // throw exception with the corresponding message
             if ($endDate == null) {
-                $message = $this->translator->trans('message.security.banned_perm', ['%reason%' => $reason]);
+                $message = 'message.security.banned_perm';
             } else {
-                $message = $this->translator->trans('message.security.banned', ['%reason%' => $reason, '%date%' => $endDate]);
+                $message = 'message.security.banned';
             }
             throw new HttpException(
                 Response::HTTP_FORBIDDEN,
-                $message
+                $this->translator->trans($message, ['%date%' => $endDate, '%reason%' => $reason])
             );
         }
     }
