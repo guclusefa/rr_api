e42b92a1370447e251df90d7a0e60d543e90f920
guclusefa <sefaaguclu@gmail.com>
2023-03-15 15:38:38 +0100

    fix command test

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ composer install --no-dev --prefer-dist --optimize-autoloader
 # Clear cache
 php bin/console cache:clear --env=prod --no-debug
 
-# Start the web server
-php -S 0.0.0.0:$PORT -t public/
+# start symfony server
+php bin/console server:start
