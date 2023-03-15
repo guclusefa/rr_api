d728b3ca87b945fec6fa8882194acbb6af2a04a2
guclusefa <sefaaguclu@gmail.com>
2023-03-15 17:21:25 +0100

    test 5

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
-# Install Composer packages
-composer install --no-dev --prefer-dist --optimize-autoloader
+#!/bin/bash
 
-# Clear cache
+# Checkout to production branch
+git checkout prod
+
+# Change to the web root directory
+cd $HOME/site/wwwroot
+
+# Clone the repository
+git clone https://github.com/guclusefa/rr_api.git rr_api
+
+# Install dependencies
+cd rr_api
+composer install --no-dev --optimize-autoloader
+
+# Clear the cache
 php bin/console cache:clear --env=prod --no-debug
 
-# Install Apache web server
-apt-get update
-apt-get install -y apache2
+# Set the permissions
+chmod -R 777 var/
 
-# Set up Apache virtual host
-cat <<EOF > /etc/apache2/sites-available/000-default.conf
-<VirtualHost *:80>
-    ServerName cesi-rr.azurewebsites.net
-    DocumentRoot /home/site/wwwroot/public
-    <Directory /home/site/wwwroot/public>
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-
-# Restart Apache web server
-service apache2 restart
+# Restart the web server
+touch $HOME/site/wwwroot/web/index.php
