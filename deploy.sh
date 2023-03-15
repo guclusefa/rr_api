aa737f3e890bedccea321a5a665ffbf35aef43a3
guclusefa <sefaaguclu@gmail.com>
2023-03-15 15:46:53 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,21 @@ composer install --no-dev --prefer-dist --optimize-autoloader
 # Clear cache
 php bin/console cache:clear --env=prod --no-debug
 
-# start symfony server
-php bin/console server:start
+# Install Apache web server
+apt-get update
+apt-get install -y apache2
+
+# Set up Apache virtual host
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+    ServerName rr-cesi.azurewebsites.net
+    DocumentRoot /home/site/wwwroot/public
+    <Directory /home/site/wwwroot/public>
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+
+# Restart Apache web server
+service apache2 restart
